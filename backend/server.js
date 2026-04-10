ebf1ed5ea0524ce32b745f98d88089aeeb9036d8
akashzone <akashnadar.dev@gmail.com>
2026-04-10 12:14:58 +0530

    feat: setup MongoDB connection with Mongoose and environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
 
 const app = express();
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+const connectDB = async () => {
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to MongoDB");
+    }catch(err){
+        console.error("Error connecting to MongoDB:", err);
+    }
+}
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // app.post("/api/generate", async (req, res) => {
@@ -45,6 +61,3 @@ app.get("/", (req, res) => {
 // //   }
 // });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
