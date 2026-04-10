6bb984f041e18424473311c7ce8070bdfe55c29e
akashzone <akashnadar.dev@gmail.com>
2026-04-10 10:00:01 +0530

    setup backend with Gemini API integration and environment configuration

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: "Explain how AI works in a few words",
+    });
+
+    console.log(response.text);
+}
+
+main();
