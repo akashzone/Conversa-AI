e5b8e8556969e180dce9b1dc5c0b8754482b75b1
akashzone <akashnadar.dev@gmail.com>
2026-04-10 10:55:07 +0530

    feat: add AI generate endpoint using Gemini API with Express

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,50 @@
 import "dotenv/config";
-import { GoogleGenAI } from "@google/genai";
+import express from "express";
+import cors from "cors";
 
-const ai = new GoogleGenAI({});
+const app = express();
 
-async function main() {
-    const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
-        contents: "Explain how AI works in a few words",
-    });
+app.use(cors());
+app.use(express.json());
 
-    console.log(response.text);
-}
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-main();
+app.post("/api/generate", async (req, res) => {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "x-goog-api-key": `${process.env.GEMINI_API_KEY}`,
+    },
+    method: "POST",
+    body: JSON.stringify({
+      model: "gemini-3-flash",
+      contents:  [
+      {
+        "role": "user",
+        parts: [
+          {
+            text: "Explain how AI works in a single paragraph."
+          }
+        ]
+      }
+    ]}),
+  };
+  try {
+    const response = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent",
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    res.json(data.candidates[0].content.parts[0].text);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
