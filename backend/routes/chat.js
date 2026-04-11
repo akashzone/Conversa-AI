55384b7bf9f56cc689b852bc24cbfe703bfeb7c7
akashzone <akashnadar.dev@gmail.com>
2026-04-11 15:13:33 +0530

    feat(threads): add endpoints to create, fetch, and delete threads

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -11,8 +11,8 @@ router.get("/", async (req, res) => {
 router.post("/threads",async (req,res)=>{
     try{
         const thread = new Thread({
-            threadId: "xyz",
-            title: "Sample Thread",
+            threadId: "abc",
+            title: "Sample Thread - 2",
         });
 
         await thread.save();
@@ -24,5 +24,50 @@ router.post("/threads",async (req,res)=>{
     }
 })
 
+// Get all threads
+router.get("/threads", async (req,res)=>{
+    
+    try{
+        const threads = await Thread.find({}).sort({updatedAt: -1});
+        if(!threads){
+            return res.status(404).json({ error: "No threads found" });
+        }
+        res.send(threads);
+    }catch(err){
+        console.error("Error fetching threads:", err);
+        res.status(500).json({ error: err.message });
+    }
+})
+
+router.get("/threads/:threadId", async (req,res)=>{
+    const { threadId } = req.params;
+    try{
+        const thread = await Thread.findOne({ threadId });
+        if(!thread){
+            return res.status(404).json({ error: "Thread not found" });
+        }
+        res.send(thread);
+    }catch(err){
+        console.error("Error fetching thread:", err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+// Delete a thread
+router.delete("threads/:threadId", async (req,res) =>{
+    const { threadId } = req.params;
+    try{
+        const deleteThread = await Thread.findOneAndDelete({ threadId });
+        if(!deleteThread){
+            return res.status(404).json({ error: "Thread not found" });
+        }
+        res.send({ message: "Thread deleted successfully" })
+
+    }catch(err){
+        console.error("Error deleting thread:", err);
+        res.status(500).json({ error: err.message });
+    }
+})
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -28,38 +28,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
-
-// app.post("/api/generate", async (req, res) => {
-// //   const options = {
-// //     headers: {
-// //       "Content-Type": "application/json",
-// //       "x-goog-api-key": `${process.env.GEMINI_API_KEY}`,
-// //     },
-// //     method: "POST",
-// //     body: JSON.stringify({
-// //       model: "gemini-3-flash",
-// //       contents:  [
-// //       {
-// //         "role": "user",
-// //         parts: [
-// //           {
-// //             text: "Explain how AI works in a single paragraph."
-// //           }
-// //         ]
-// //       }
-// //     ]}),
-// //   };
-// //   try {
-// //     const response = await fetch(
-// //       "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent",
-// //       options
-// //     );
-// //     const data = await response.json();
-// //     // console.log(data);
-// //     res.json(data.candidates[0].content.parts[0].text); //reply
-// //   } catch (error) {
-// //     console.error(error);
-// //     res.status(500).json({ error: error.message });
-// //   }
-// });
-
