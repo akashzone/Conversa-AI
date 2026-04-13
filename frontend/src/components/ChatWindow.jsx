7c2c518d860904de15e48e6cd1f0fb151139463a
akashzone <akashnadar.dev@gmail.com>
2026-04-13 11:22:36 +0530

    feat(ui): complete styling for navbar, chat area, and input section

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './ChatWindow.css'
 const ChatWindow = () => {
   return (
-    <div>ChatWindow</div>
+    <div className="mainChatWindow">
+      <div className="navbar">
+        <span className="appName">Conversa AI <i class="fa-solid fa-angle-down"></i></span>
+        <div className="userProfile">
+          <i class="fa-solid fa-user userIcon"></i>
+        </div>
+      </div>
+      <div className="chatArea">
+      
+      </div>
+      <div className="inputArea">
+      <div className="inputBox">
+        <input type="text" name="" id="" placeholder="Ask anything..."/>
+        <button className="sendBtn"><i class="fa-solid fa-paper-plane sendIcon"></i></button>
+      </div>
+      <div className="footer">
+          <span>Conversa AI can make mistakes. Check important info. <a href="#">See Cookie Preferences.</a></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
