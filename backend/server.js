9a15de42660a41f7eeeb59dded2a8257fc9bffd1
akashzone <akashnadar.dev@gmail.com>
2026-04-17 15:29:26 +0530

    Deploy backend on Render

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.listen(PORT, async () => {
   try {
     await connectDB();
